14bcc2b85cf62c09abfe8bae2dbaed2ce92a26f1
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-04-28 13:32:15 -0400

    More refactoring

---
+++ test/fixtures/elixir-crypto-readme.md
@@ -0,0 +1,7 @@
+## Cryptography
+*Encrypting and decrypting data*
+
+* [elixir_tea](https://github.com/keichan34/elixir_tea) - TEA implementation in Elixir.
+* [exgpg](https://github.com/rozap/exgpg) - Use gpg from Elixir.
+* [pot](https://github.com/yuce/pot) - Erlang library for generating one time passwords compatible with Google Authenticator.
+* [tea_crypto](https://github.com/keichan34/tea_crypto_erl) - Tiny Encryption Algorithm implementation.
--- test/starcount_spec.js
+++ test/starcount_spec.js
@@ -1,33 +1,29 @@
 var expect = require('chai').expect;
+process.env['TOKEN'] = 'foobar';
 var starcount = require('../lib/starcount'),
     nock = require('nock');
 
 describe('starcount', function() {
   beforeEach(function() {
-    this.repoUrl = 'https://github.com/mikespook/gorbac';
+    this.repoUrl = {user: 'mikespook', repo: 'gorbac'};
     this.githubReq = nock('https://api.github.com')
-      .get('/repos/mikespook/gorbac')
-      .replyWithFile(200, __dirname + '/fixtures/mikespook-gorbac.json');
+      .get('/repos/mikespook/gorbac?access_token=foobar')
+      .reply(200, {stargazers_count: 82});
   });
 
   describe('when the repo url is garbage', function() {
     it('resolves an empty promise', function(done) {
-      starcount('#foobar').then(function(count) {
+      starcount(null).then(function(count) {
         expect(count).to.be.undefined;
         done();
       });
     });
   });
 
-  it('retrieves the github stargazer count for the repo url', function(done) {
+  it.only('retrieves the github stargazer count for the repo url', function(done) {
     starcount(this.repoUrl).then(function(count) {
       expect(count).to.equal(82);
       done();
     });
-    // reader.readMarkdown().then(function(contents) {
-    //   expect(contents).to.be.instanceof(Object);
-    //   expect(contents).to.have.all.keys(['node', 'golang']);
-    //   done();
-    // });
   });
 });
