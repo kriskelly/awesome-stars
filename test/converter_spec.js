e94fc701a117d5148f151a717b82a8e885eb1fa5
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-05-14 10:47:43 -0400

    Fix another spec

--- test/converter_spec.js
+++ test/converter_spec.js
@@ -2,20 +2,22 @@ var expect = require('chai').expect;
 var fs = require('fs');
 var proxyquire = require('proxyquire');
 var Promise = require('bluebird');
+var sinon = require('sinon');
+require('sinon-bluebird');
 var _ = require('lodash');
 process.env['TOKEN'] = 'foobar';
-var stubs = {
-  './starcount': function() {
-    return Promise.resolve(123); // Always return 123 for the star count.
-  }
-}
-var converter = proxyquire('../lib/converter.js', stubs);
+var starcountStub = sinon.stub();
+var converter = proxyquire('../lib/converter.js', {
+  './starcount': starcountStub,
+  './datastore': {},
+});
 var pretty = require('prettyjson');
 var nock = require('nock');
 var marked = require('marked');
 
 describe('converting list to table', function() {
   beforeEach(function() {
+    starcountStub.resolves(123);
     this.content = fs.readFileSync('./test/fixtures/nodejs-readme.md').toString();
   });
 
@@ -27,18 +29,6 @@ describe('converting list to table', function() {
     });
   });
 
-  // describe.only('with tables in the parsed markdown', function() {
-  //   beforeEach(function() {
-  //     this.tableContent = fs.readFileSync('./test/fixtures/table.md').toString();
-  //   });
-
-  //   it('parses tables', function() {
-  //     var tree = converter.getTree(this.tableContent);
-  //     console.log(tree[0]);
-  //     // expect(tree[1][1][0]).to.equal('thead');
-  //   });
-  // });
-
   describe('with underscores in the repo names', function() {
     beforeEach(function() {
       this.content = fs.readFileSync('./test/fixtures/elixir-crypto-readme.md').toString();
@@ -46,8 +36,8 @@ describe('converting list to table', function() {
 
     it('converts the markdown correctly', function(done) {
       var tree = converter.getTree(this.content);
-      converter.convertListToTable(tree).then(function() {
-        var html = converter.toHTML(tree);
+      converter.convertListToTable(tree).then(function(newTree) {
+        var html = converter.toHTML(newTree);
         expect(html).to.include('elixir_tea');
         done();
       });
